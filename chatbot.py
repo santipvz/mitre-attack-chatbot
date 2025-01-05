52e7784341875c2e3d08de37ca31eb3277f0168f
santipvz <santivzqzz@gmail.com>
2025-01-05 02:51:09 +0100

    Refactor and explained functions

--- chatbot.py
+++ chatbot.py
@@ -1,25 +1,36 @@
+"""
+Implementación de un chatbot experto en MITRE ATT&CK con OpenAI, Langchain y FAISS.
+"""
+
 import os
 import sys
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
 
 # Cargar variables de entorno
 load_dotenv()
-embedding_model = OpenAIEmbeddings(model="text-embedding-3-small", api_key=os.environ["OPENAI_API_KEY"])
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small",
+                                   api_key=os.environ["OPENAI_API_KEY"])
 
 # Cargar el índice previamente generado
 def load_index():
+    """
+    Carga el índice previamente generado desde el almacenamiento local.
+
+    Returns:
+        local_vector_store: El índice cargado desde el almacenamiento local.
+    """
     try:
-        vector_store = FAISS.load_local("vector_store_index", embedding_model, allow_dangerous_deserialization=True)
+        local_vector_store = FAISS.load_local("vector_store_index",
+                                              embedding_model, allow_dangerous_deserialization=True)
         print("Índice cargado correctamente.")
-        return vector_store
-    except Exception as e:
-        print("Error al cargar el índice:", e)
+        return local_vector_store
+    except (FileNotFoundError, ValueError) as error:
+        print("Error al cargar el índice:", error)
         sys.exit("Asegúrate de haber generado el índice correctamente con el módulo de indexación.")
 
 vector_store = load_index()
@@ -32,6 +43,15 @@ workflow = StateGraph(state_schema=MessagesState)
 
 # Función que llama al modelo
 def call_model(state: MessagesState):
+    """
+    Llama al modelo de lenguaje con el estado actual de los mensajes.
+
+    Args:
+        state (MessagesState): El estado actual de los mensajes.
+
+    Returns:
+        dict: Un diccionario con el historial de mensajes actualizado.
+    """
     response = llm.invoke(state["messages"])
     # Actualizar el historial de mensajes con la respuesta
     return {"messages": response}
@@ -48,16 +68,23 @@ app = workflow.compile(checkpointer=memory)
 config = {"configurable": {"thread_id": "1111"}}
 
 # System prompt específico para MITRE ATT&CK
-texto = ("Eres un asistente experto en MITRE ATT&CK. "
-         "Proporcionas información clara y precisa sobre técnicas de ataque, tácticas y contramedidas basadas en la base de conocimientos de MITRE. "
-         "También puedes recordar detalles relevantes de la conversación para responder de manera más eficiente y contextualizada. "
+TEXTO = ("Eres un asistente experto en MITRE ATT&CK. "
+         "Proporcionas información clara y precisa sobre técnicas de ataque,"
+         " tácticas y contramedidas basadas en la base de conocimientos de MITRE. "
+         "También puedes recordar detalles relevantes de la conversación "
+         " para responder de manera más eficiente y contextualizada. "
          "Usa un lenguaje sencillo y directo para ahorrar tokens.")
-prompt_base = SystemMessage(texto)
+prompt_base = SystemMessage(TEXTO)
 # Cargar el system prompt inicial en la memoria
-output = app.invoke({"messages": [prompt_base]}, config)
+initial_output = app.invoke({"messages": [prompt_base]}, config)
 
 # Función principal para el chatbot
 def main():
+    """
+    Función principal para el chatbot experto en MITRE ATT&CK.
+    Maneja la entrada del usuario y proporciona respuestas basadas
+    en el grafo de estados configurado.
+    """
     print("\n======= CHATBOT EXPERTO EN MITRE ATT&CK =======")
     print("Hazme preguntas sobre tácticas, técnicas o contramedidas.")
     print("Finaliza la sesión con los comandos :salir, :exit o :terminar.")
@@ -70,9 +97,9 @@ def main():
             sys.exit()
 
         input_messages = [HumanMessage(query)]
-        output = app.invoke({"messages": input_messages}, config)
+        response = app.invoke({"messages": input_messages}, config)
         print("\nAsistente:", end=" ")
-        output["messages"][-1].pretty_print()  # Mostrar la última respuesta
+        response["messages"][-1].pretty_print()  # Mostrar la última respuesta
 
 if __name__ == "__main__":
     main()
--- index.py
+++ index.py
@@ -1,16 +1,27 @@
+"""
+Este módulo indexa datos de un archivo JSON y almacena embeddings en un almacén vectorial FAISS.
+"""
+
+import os
 import json
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.schema import Document
-import os
 from dotenv import load_dotenv
 
 # Cargar variables de entorno
 load_dotenv()
-embedding_model = OpenAIEmbeddings(model="text-embedding-3-small", api_key=os.environ["OPENAI_API_KEY"])
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small",
+                                   api_key=os.environ["OPENAI_API_KEY"])
 
 def index_data(file_path):
-    with open(file_path, "r") as file:
+    """
+    Indexa datos de un archivo JSON y almacena embeddings en un almacén vectorial FAISS.
+
+    Args:
+        file_path (str): Ruta al archivo JSON que contiene los datos a indexar.
+    """
+    with open(file_path, "r", encoding='utf-8') as file:
         data = json.load(file)
 
     documents = []
